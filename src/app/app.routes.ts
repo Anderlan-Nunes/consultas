2974ab7f8a92bb7594757cfce356c94a47768ed8
Anderlan pc1 <anderlan.dev@gmail.com>
2024-02-22 10:30:05 -0300

    alterando as cores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'especialidades' },
+  { path: '', pathMatch: 'full', redirectTo: 'especialidades' }, // como tem o redirecionamento o angula ja carrega este component
   {
     path: 'especialidades',
     loadChildren: () => import('./especialidades/especialidades.routes').then(m => m.ESPECIALIDADES_ROUTES)
